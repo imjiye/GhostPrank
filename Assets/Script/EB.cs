a4fbd0ce436863ce83584f358e028c699ab77073
imjiye <imjyjy17@gmail.com>
2024-07-12 11:12:47 +0900

    24.07.12 수정

---
+++ Assets/Script/EB.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EB : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    Rigidbody rd;
+
+    public GameObject effect1;
+    public GameObject effect2;
+
+    public AudioClip sound1;
+    public AudioSource audio;
+
+    public MeshRenderer renderer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerController pc = other.GetComponent<PlayerController>();
+            if (pc != null)
+            {
+                pc.cnt--;
+                pc.UpdateUI();
+                audio.PlayOneShot(sound1);
+                Instantiate(effect1, transform.position, Quaternion.identity);
+                Destroy(gameObject, 1f);
+                renderer.enabled = false;
+
+
+                if (pc.cnt == 0)
+                {
+                    audio.PlayOneShot(sound1);
+                    Instantiate(effect2, transform.position, Quaternion.identity);
+                    pc.Die();
+                }
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd = GetComponent<Rigidbody>();
+        rd.velocity = transform.forward * speed;
+        renderer = GetComponent<MeshRenderer>();
+
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
